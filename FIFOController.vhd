a627d494c9bf5fa97632e9847c43d4556372d5b0
Patrick Hultberg <90772733+Cicerotheprogrammer@users.noreply.github.com>
2026-04-21 14:19:54 -0400

    Add files via upload

--- FIFOController.vhd
+++ FIFOController.vhd
@@ -43,8 +43,7 @@ Port (
     RST: in std_logic;
     r_addr: in std_logic_vector(3 downto 0);
     w_data: in std_logic_vector(7 downto 0);
-    r_data: out std_logic_vector(7 downto 0);
-    r_addr0: out std_logic_vector(3 downto 0)
+    r_data: out std_logic_vector(7 downto 0)
    
     
     
@@ -57,34 +56,39 @@ signal w_en:  std_logic;
 signal full1: std_logic; 
 signal w_addr_int: unsigned(3 downto 0) := (others => '0');
 signal r_addr_int: unsigned(3 downto 0) := (others => '0');
+type memory is array(0 to 15) of std_logic_vector(7 downto 0);
+signal ram: memory := (others =>(others => '0'));
+signal empty1: std_logic;
 begin
 
-w_addr_cont <= "0000";
-full <= '0' when wr = '1' else '1';
-full1 <= '0' when wr = '1' else '1';
-empty <= '1' when full1 = '0' else '0';
+
+full1 <= '0' when wr = '1' and w_addr_int < 8 and r_addr_int /= w_addr_int else '1';
+empty1 <= '1' when full1 = '0' else '0';
+
 w_en <= wr and not full1; 
 process(w_en, CLK, RST)
 begin
-if (w_en = '1' and rising_edge(CLK)) then
-    w_addr_int <= w_addr_int + 1;     
-elsif (RST = '1' and rising_edge(CLK)) then
+if (RST = '1') then
     w_addr_int <= "0000";
+    r_addr_int <= "0000";
+    r_data <= "00000000";
+elsif (rising_edge(CLK)) then
+
+if (w_en = '1' and full1 = '0') then
+    ram(TO_INTEGER(w_addr_int)) <= w_data;
+    w_addr_int <= w_addr_int + 1;
+end if;     
+if (rd = '1' and empty1 = '1') then
+    r_data <= ram(TO_INTEGER(r_addr_int));
+    r_addr_int <= r_addr_int + 1;
+    
+   
+end if;
+
 end if;
 end process;
-process(rd, CLK, RST)
-begin
-
-if (rd = '1' and rising_edge(CLK)) then
-    r_addr_int <= w_addr_int;
-    r_data <= w_data;
-    w_addr_int <= w_addr_int - 1;
-    
-elsif (RST = '1' and rising_edge(CLK)) then
-    r_addr_int <= "0000";
-end if; 
-end process;
-
+full <= full1;
+empty <= empty1;
 w_addr_cont <= std_logic_vector(w_addr_int);
 r_addr_cont <= std_logic_vector(r_addr_int);
 end Behavioral;
