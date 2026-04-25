28f24a62d803792475e626eb15b370168a71f9ca
Patrick Hultberg <90772733+Cicerotheprogrammer@users.noreply.github.com>
2026-04-25 09:30:56 -0400

    Add files via upload

--- FIFOController.vhd
+++ FIFOController.vhd
@@ -41,10 +41,10 @@ Port (
     r_addr_cont: out std_logic_vector(3 downto 0);
     CLK: in std_logic;
     RST: in std_logic;
-    r_addr: in std_logic_vector(3 downto 0);
+--    r_addr: in std_logic_vector(3 downto 0);
     w_data: in std_logic_vector(7 downto 0);
     r_data: out std_logic_vector(7 downto 0)
-   
+
     
     
  );
@@ -52,43 +52,64 @@ end FIFOController;
 
 architecture Behavioral of FIFOController is
 
-signal w_en:  std_logic;
+--signal w_en:  std_logic;
 signal full1: std_logic; 
 signal w_addr_int: unsigned(3 downto 0) := (others => '0');
 signal r_addr_int: unsigned(3 downto 0) := (others => '0');
 type memory is array(0 to 15) of std_logic_vector(7 downto 0);
 signal ram: memory := (others =>(others => '0'));
 signal empty1: std_logic;
+signal count: integer range 0 to 16 := 0;
 begin
 
 
-full1 <= '0' when wr = '1' and w_addr_int < 8 and r_addr_int /= w_addr_int else '1';
-empty1 <= '1' when full1 = '0' else '0';
 
-w_en <= wr and not full1; 
-process(w_en, CLK, RST)
-begin
-if (RST = '1') then
-    w_addr_int <= "0000";
-    r_addr_int <= "0000";
-    r_data <= "00000000";
-elsif (rising_edge(CLK)) then
-
-if (w_en = '1' and full1 = '0') then
-    ram(TO_INTEGER(w_addr_int)) <= w_data;
-    w_addr_int <= w_addr_int + 1;
-end if;     
-if (rd = '1' and empty1 = '1') then
-    r_data <= ram(TO_INTEGER(r_addr_int));
-    r_addr_int <= r_addr_int + 1;
-    
-   
-end if;
-
-end if;
-end process;
+full1 <= '1' when count = 16 else '0';
+empty1 <= '1' when count = 0 else '0';
 full <= full1;
 empty <= empty1;
+--r_data <= ram(TO_INTEGER(r_addr_int)) when empty1 = '0' else (others => '0');
+
+process(CLK, RST)
+begin
+if (RST = '1') then
+    w_addr_int <= (others => '0');
+    r_addr_int <= (others => '0');
+    count      <= 0;
+--    r_data     <= (others => '0');
+elsif (rising_edge(CLK)) then
+if (wr = '1' and full1 = '0') and (rd = '1' and empty1 = '0') then
+                w_addr_int <= w_addr_int + 1;
+                r_addr_int <= r_addr_int + 1;
+
+elsif (wr = '1' and full1 = '0') then
+    w_addr_int <= w_addr_int + 1;
+--    ram[i] = w_data
+     count <= count + 1;
+     
+elsif (rd = '1' and empty1 = '0') then
+--    r_data <= ram(TO_INTEGER(r_addr_int));
+--    r_data = ram[i]
+    r_addr_int <= r_addr_int + 1;
+    count <= count-1;
+   
+end if;
+end if;
+
+end process;
+process(CLK)
+begin
+    if rising_edge(CLK) then
+            if (wr = '1' and full1 = '0') then
+                ram(TO_INTEGER(w_addr_int)) <= w_data;
+            end if;
+            
+            if (rd = '1' and empty1 = '0') then
+                r_data <= ram(TO_INTEGER(r_addr_int));
+            end if;
+        end if;
+end process;
+
 w_addr_cont <= std_logic_vector(w_addr_int);
 r_addr_cont <= std_logic_vector(r_addr_int);
 end Behavioral;
---
+++ FIFOController_tb.vhd
@@ -0,0 +1,135 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/21/2026 01:07:15 PM
+-- Design Name: 
+-- Module Name: FIFOController_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FIFOController_tb is
+--  Port ( );
+end FIFOController_tb;
+
+architecture Behavioral of FIFOController_tb is
+signal wr: std_logic := '0';
+signal rd: std_logic := '0';
+signal full: std_logic;
+signal empty: std_logic;
+signal w_addr_cont: std_logic_vector(3 downto 0);
+signal r_addr_cont: std_logic_vector(3 downto 0);
+signal CLK: std_logic;
+signal RST: std_logic;
+--signal r_addr: std_logic_vector(3 downto 0);
+signal w_data: std_logic_vector(7 downto 0);
+signal r_data: std_logic_vector(7 downto 0);
+signal w_addr: std_logic_vector(3 downto 0);
+constant clk_period : time := 10 ns;
+
+component FIFOController
+port(
+    wr: in std_logic;
+    rd: in std_logic;
+    full: out std_logic;
+    empty: out std_logic;
+    w_addr_cont: out std_logic_vector(3 downto 0);
+    r_addr_cont: out std_logic_vector(3 downto 0);
+    CLK: in std_logic;
+    RST: in std_logic;
+--    r_addr: in std_logic_vector(3 downto 0);
+    w_data: in std_logic_vector(7 downto 0);
+    r_data: out std_logic_vector(7 downto 0)
+
+);
+
+end component;
+begin
+UUT: FIFOController
+    port map(
+        CLK => CLK,
+        RST => RST,
+        wr => wr,
+        rd => rd,
+        full => full,
+        empty => empty,
+        w_addr_cont => w_addr_cont,
+        r_addr_cont => r_addr_cont,
+--        r_addr => r_addr,
+        w_data => w_data,
+        r_data => r_data
+
+        
+    );
+ clk_process : process
+  begin
+    CLK <= '0';
+    wait for clk_period / 2;  
+    CLK <= '1';
+    wait for clk_period / 2;  
+end process;
+sim:    process begin
+    RST <= '1';
+    wr <= '0';
+    rd <= '0';
+    w_data <= (others => '0');
+    wait for 20 ns;	
+    RST <= '0';
+    wait for clk_period;
+    wr <= '1';
+    for i in 1 to 16 loop
+        w_data <= std_logic_vector(TO_UNSIGNED(i*10, 8));
+        wait for clk_period;
+    end loop;
+    w_data <= x"FF";
+    rd <= '1';
+    for i in 1 to 16 loop
+         wait for clk_period*2;
+    end loop;
+    
+    wait for clk_period;
+    rd <= '0';
+    wait for clk_period * 2;
+    
+    wait until falling_edge(CLK);
+    w_data <= x"AA";
+     wr <= '1';
+    wait until falling_edge(CLK);
+    w_data <= x"BB";
+        
+    -- Now read and write at the exact same time
+    wr <= '1';
+    w_data <= x"CC"; 
+    rd <= '1'; 
+    wait for clk_period;
+        
+    wr <= '0';
+    rd <= '0';
+    wait for 50 ns;
+    wait;
+    end process;
+
+end Behavioral;
